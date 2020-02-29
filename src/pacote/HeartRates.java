a89895815e1a36850d93d89c8570530ead1fb8a7
rafaelalves271 <61068108+rafaelalves271@users.noreply.github.com>
2020-02-29 19:10:48 -0300

    getIdade() funcionando

--- src/pacote/HeartRates.java
+++ src/pacote/HeartRates.java
@@ -4,34 +4,35 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class HeartRates {
-	String nome;
-	String sobrenome;
-	int diaNasc;
-	int mesNasc;
-	int anoNasc;
-	
-	public HeartRates(String nome, String sobrenome, int diaNasc, int mesNasc, int anoNasc) {
-		this.nome = nome;
-		this.sobrenome = sobrenome;
-		this.diaNasc = diaNasc;
-		this.mesNasc = mesNasc;
-		this.anoNasc = anoNasc;
-	}
-	
-	public int getIdade() {
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy");
-		LocalDate localDate = LocalDate.now();
-		int anoAtual = Integer.parseInt(dtf.format(localDate));
-		dtf = DateTimeFormatter.ofPattern("MM");
-		int mesAtual = Integer.parseInt(dtf.format(localDate));
-		dtf = DateTimeFormatter.ofPattern("dd");
-		int diaAtual = Integer.parseInt(dtf.format(localDate));
-		int idadeTotal = anoAtual - anoNasc;
-		
-		if (mesAtual >= mesNasc && diaAtual >= diaNasc) {
-			idadeTotal++;
-		}
-		
-		return idadeTotal;
-	}
+    String nome;
+    String sobrenome;
+    int diaNasc;
+    int mesNasc;
+    int anoNasc;
+
+    public HeartRates(String nome, String sobrenome, int diaNasc, int mesNasc, int anoNasc) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.diaNasc = diaNasc;
+        this.mesNasc = mesNasc;
+        this.anoNasc = anoNasc;
+    }
+
+    public int getIdade() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy");
+        LocalDate localDate = LocalDate.now();
+        int anoAtual = Integer.parseInt(dtf.format(localDate));
+        dtf = DateTimeFormatter.ofPattern("MM");
+        int mesAtual = Integer.parseInt(dtf.format(localDate));
+        dtf = DateTimeFormatter.ofPattern("dd");
+        int diaAtual = Integer.parseInt(dtf.format(localDate));
+        int idadeTotal = anoAtual - anoNasc;
+        idadeTotal--; //Já tira um ano, se a pessoa já tiver feito aniversário, que irá ser somado de novo
+
+        if ((mesAtual >= mesNasc) && (diaAtual >= diaNasc)) {
+            idadeTotal++;
+        }
+
+        return idadeTotal;
+    }
 }
